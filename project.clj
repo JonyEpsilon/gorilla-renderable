e3121504d57f6bee83e07bea55f518e95e0c7881
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-10-24 15:00:57 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pinkgorilla.ui.gorilla-renderable "2.0.11"
+(defproject pinkgorilla.ui.gorilla-renderable "2.0.12"
   :description "The protocol for custom rendering in gorilla REPL."
   :url "https://github.com/pink-gorilla/gorilla-renderable"
   :license {:name "MIT"}
