8122b8397c24089936e846619982fc030fbdb108
Andreas Steffan <a.steffan@contentreich.de>
2019-11-03 07:46:25 +0100

    At least compile in test
    Roll to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable "2.0.13"
+(defproject org.pinkgorilla/gorilla-renderable "2.0.14-SNAPSHOT"
   :description "The protocol for custom rendering in gorilla REPL."
   :url "https://github.com/pink-gorilla/gorilla-renderable"
   :license {:name "MIT"}
@@ -9,7 +9,7 @@
                              :sign-releases false}]]
   
   :dependencies 
-  [[org.clojure/clojure "1.9.0-alpha14"]
+  [[org.clojure/clojure "1.10.1"]
    [org.clojure/data.json "0.2.6"] ; used by old vega renderer
    [hiccup "1.0.5"] ; used in hiccup rendering
    ])
