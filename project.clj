154a845d913c667a5aa82e36e0692e5344c4d25a
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-11-23 17:43:04 -0600

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable "2.1.1"
+(defproject org.pinkgorilla/gorilla-renderable "2.1.2"
   :description "The protocol for custom rendering in Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/gorilla-renderable"
   :license {:name "MIT"}
