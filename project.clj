d1a61abd6900f5e4bda354e0e5ce3b7421edbdc8
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-01-02 23:06:31 -0500

    unit test updated

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable "2.1.18"
+(defproject org.pinkgorilla/gorilla-renderable "2.1.19"
   :description "The protocol for custom rendering in Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/gorilla-renderable"
   :license {:name "MIT"}
--- test/pinkgorilla/ui/to_reagent_test.cljs
+++ test/pinkgorilla/ui/to_reagent_test.cljs
@@ -2,10 +2,16 @@
   (:require
    [cljs.test :refer-macros [async deftest is testing]]
    [pinkgorilla.ui.reagent-keyword :refer [keyword-to-reagent unknown-renderer]]
+   [pinkgorilla.ui.pinkie :refer [register-tag]]
    ))
 
 
 
-(deftest to-reagent-pp
+(deftest pinkie-register
   (is (= (keyword-to-reagent :BONGO :a 4)
          unknown-renderer)))
+
+
+(deftest pinkie-register
+  (is (= (register-tag :bongo nil)
+         {:bongo nil})))
