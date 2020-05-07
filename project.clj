6c9ba9ada082fbea984435a0c90e886af4a6cc83
CI/CD <ci@pinkgorilla.org>
2020-05-07 19:45:15 +0000

    Release 3.0.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable "3.0.8-SNAPSHOT"
+(defproject org.pinkgorilla/gorilla-renderable "3.0.8"
   :description "The protocol for custom rendering in Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/gorilla-renderable"
   :license {:name "MIT"}
