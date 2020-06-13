0ff891b59734696c2c4bc39f4c7f2c50d28c0b39
CI/CD <ci@pinkgorilla.org>
2020-06-13 18:01:25 +0000

    Release 3.0.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable "3.0.13-SNAPSHOT"
+(defproject org.pinkgorilla/gorilla-renderable "3.0.13"
   :description "renderer lookup engine"
   :url "https://github.com/pink-gorilla/gorilla-renderable"
   :license {:name "MIT"}
