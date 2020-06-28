a395dcc4f5b679f76b779a536eae22e7f13e2423
CI/CD <ci@pinkgorilla.org>
2020-06-28 18:56:36 +0000

    Release 3.1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/picasso "3.1.9-SNAPSHOT"
+(defproject org.pinkgorilla/picasso "3.1.9"
   :description "renderer lookup engine"
   :url "https://github.com/pink-gorilla/picasso"
   :license {:name "MIT"}
