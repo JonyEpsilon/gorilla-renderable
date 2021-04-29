152a46fb1d8be5aa640bedfc9537d23761f8d065
CI/CD <ci@pinkgorilla.org>
2021-04-29 04:07:56 +0000

    Begin 3.1.22-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/picasso "3.1.21"
+(defproject org.pinkgorilla/picasso "3.1.22-SNAPSHOT"
   :description "renderer lookup engine"
   :url "https://github.com/pink-gorilla/picasso"
   :license {:name "MIT"}
